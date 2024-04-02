4c85e7ea6728b74d5ac097fbb70ff6f36b2051df
Luan <luanhb7@gmail.com>
2024-04-02 16:44:30 -0300

    feat: connect db and server

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running in port 3000');
+  console.log('Server is running in port 3000!');
 });
